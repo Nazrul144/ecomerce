801244eaa5d0ed421e89f3e99197b731a0ef23a5
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-03 01:12:09 +0600

    See more button functionality added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,16 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 import Image from "next/image";
 import Link from "next/link";
-
+import { Button } from "@mui/material";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [visibleProducts, setVisibleProducts] = useState(6)
   useEffect(() => {
     const getAllProducts = async () => {
       const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/products/api`);
@@ -19,14 +20,17 @@ const Products = () => {
     getAllProducts();
   }, []);
 
+  const handleIncrement = ()=>{
+    setVisibleProducts(visibleProducts +2)
+  }
 
   return (
     <div className="w-[1100px] mx-auto">
       <h1>This is my product page</h1>
       <div className="grid grid-cols-3 gap-6">
-        {products?.map((product) => (
+        {products?.slice(0, visibleProducts).map((product) => (
           <Card key={product._id} sx={{ maxWidth: 345 }}>
-            <Image  src={product.image} height={200} width={300} alt="Image"/>
+            <Image src={product.image} height={200} width={300} alt="Image" />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
                 <h1>{product.name}</h1>
@@ -41,6 +45,11 @@ const Products = () => {
             </CardActions>
           </Card>
         ))}
+        <div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center mt-3">
+      <Button onClick={handleIncrement} color="secondary">See More</Button>
       </div>
     </div>
   );
