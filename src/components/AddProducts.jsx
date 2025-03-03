f612e95191405a9f727f249d1e8005abf5fc4773
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-03 17:38:15 +0600

    Add some meaningful comments

--- src/components/AddProducts.jsx
+++ src/components/AddProducts.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 
 const AddProducts = () => {
 
+    /*How to store data form client side to mongodb database?
+    Step1: First create a route with name like addItems or addProducts and in the route folder create another folder name api and in the api you must create a file route.js. And in the route.js file you have to write a function name export const POST async since you have to post data to the mongodb.
+
+    Then form client side, you have to write an async function and in the async function you have to create an object to bind all data that you have collected by a form. Now let's send the data to backend by a method. you have to fetch the backend route now. Like const response await = fetch (write the name of the route, {method: "POST"}, header:{}). and body: JSON.stringify(items). Now you also can console the response easily. How easy the process is write.
+    */
+
+
     const handleAddItems = async(event)=>{
         event.preventDefault()
         const items = {
